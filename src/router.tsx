03286c419eb2e12a5b8ae377a72e7af627baff46
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2026-04-19 16:47:37 +0200

    feat(fe): add page for forgotten password to router

--- src/router.tsx
+++ src/router.tsx
@@ -4,6 +4,7 @@ import { PublicLayout } from "./routes/layouts/public-layout";
 import { AdminEventPage } from "./routes/pages/admin-event-page";
 import { AdminEventsNewPage } from "./routes/pages/admin-events-new-page";
 import { AdminEventsPage } from "./routes/pages/admin-events-page";
+import { AdminForgotPasswordPage } from "./routes/pages/admin-forgot-password-page";
 import { AdminLoginPage } from "./routes/pages/admin-login-page";
 import { AdminPage } from "./routes/pages/admin-page";
 import { EventPage } from "./routes/pages/event-page";
@@ -13,49 +14,26 @@ import { NotFoundPage } from "./routes/pages/not-found-page";
 export const router = createBrowserRouter([
   {
     children: [
-      {
-        Component: HomePage,
-        index: true,
-      },
-      {
-        Component: EventPage,
-        path: "events/:eventId",
-      },
+      { Component: HomePage, index: true },
+      { Component: EventPage, path: "events/:eventId" },
     ],
     element: <PublicLayout />,
   },
   {
     children: [
-      {
-        Component: AdminLoginPage,
-        path: "login",
-      },
+      { Component: AdminForgotPasswordPage, path: "forgot-password" },
+      { Component: AdminLoginPage, path: "login" },
       {
         children: [
-          {
-            Component: AdminPage,
-            index: true,
-          },
-          {
-            Component: AdminEventsPage,
-            path: "events",
-          },
-          {
-            Component: AdminEventsNewPage,
-            path: "events/new",
-          },
-          {
-            Component: AdminEventPage,
-            path: "events/:eventId",
-          },
+          { Component: AdminPage, index: true },
+          { Component: AdminEventsPage, path: "events" },
+          { Component: AdminEventsNewPage, path: "events/new" },
+          { Component: AdminEventPage, path: "events/:eventId" },
         ],
         element: <AdminLayout />,
       },
     ],
     path: "admin",
   },
-  {
-    Component: NotFoundPage,
-    path: "*",
-  },
+  { Component: NotFoundPage, path: "*" },
 ]);
---
+++ src/routes/pages/admin-forgot-password-page.tsx
@@ -0,0 +1,5 @@
+import { PagePlaceholder } from "../components/page-placeholder";
+
+export function AdminForgotPasswordPage() {
+  return <PagePlaceholder title="Admin Forgot Password Page" />;
+}
