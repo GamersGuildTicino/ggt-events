ba148b18140a40f67b0b750ae21e36c7ba1cb16f
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2026-04-19 11:58:52 +0200

    style: fix linting in all ts files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import reactLogo from "~/assets/react.svg";
-import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
+import viteLogo from "./assets/vite.svg";
 import "./App.css";
 
 function App() {
@@ -11,9 +11,9 @@ function App() {
     <>
       <section id="center">
         <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+          <img alt="" className="base" height="179" src={heroImg} width="170" />
+          <img alt="React logo" className="framework" src={reactLogo} />
+          <img alt="Vite logo" className="vite" src={viteLogo} />
         </div>
         <div>
           <h1>Get started</h1>
@@ -33,7 +33,7 @@ function App() {
 
       <section id="next-steps">
         <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
+          <svg aria-hidden="true" className="icon" role="presentation">
             <use href="/icons.svg#documentation-icon"></use>
           </svg>
           <h2>Documentation</h2>
@@ -41,20 +41,20 @@ function App() {
           <ul>
             <li>
               <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
+                <img alt="" className="logo" src={viteLogo} />
                 Explore Vite
               </a>
             </li>
             <li>
               <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
+                <img alt="" className="button-icon" src={reactLogo} />
                 Learn more
               </a>
             </li>
           </ul>
         </div>
         <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
+          <svg aria-hidden="true" className="icon" role="presentation">
             <use href="/icons.svg#social-icon"></use>
           </svg>
           <h2>Connect with us</h2>
@@ -63,9 +63,9 @@ function App() {
             <li>
               <a href="https://github.com/vitejs/vite" target="_blank">
                 <svg
+                  aria-hidden="true"
                   className="button-icon"
                   role="presentation"
-                  aria-hidden="true"
                 >
                   <use href="/icons.svg#github-icon"></use>
                 </svg>
@@ -75,9 +75,9 @@ function App() {
             <li>
               <a href="https://chat.vite.dev/" target="_blank">
                 <svg
+                  aria-hidden="true"
                   className="button-icon"
                   role="presentation"
-                  aria-hidden="true"
                 >
                   <use href="/icons.svg#discord-icon"></use>
                 </svg>
@@ -87,9 +87,9 @@ function App() {
             <li>
               <a href="https://x.com/vite_js" target="_blank">
                 <svg
+                  aria-hidden="true"
                   className="button-icon"
                   role="presentation"
-                  aria-hidden="true"
                 >
                   <use href="/icons.svg#x-icon"></use>
                 </svg>
@@ -99,9 +99,9 @@ function App() {
             <li>
               <a href="https://bsky.app/profile/vite.dev" target="_blank">
                 <svg
+                  aria-hidden="true"
                   className="button-icon"
                   role="presentation"
-                  aria-hidden="true"
                 >
                   <use href="/icons.svg#bluesky-icon"></use>
                 </svg>
