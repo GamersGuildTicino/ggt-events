f20fdfad55fd79f5dc8b722ea1b5c42b0491e526
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2026-04-19 13:28:38 +0200

    feat(fe): wrap app with ThemeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "~/app";
+import App from "./app";
+import { ThemeProvider } from "./theme/theme-provider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
