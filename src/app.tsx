d16f795ccaa2a22e89b5ba743a3a8e371f1c5f23
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2026-04-19 22:08:03 +0200

    refactor(fe): move AuthProvider in main.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,6 @@
 import { RouterProvider } from "react-router";
-import AuthProvider from "./auth/auth-provider";
 import { router } from "./router";
 
 export default function App() {
-  return (
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
-  );
+  return <RouterProvider router={router} />;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app";
+import AuthProvider from "./auth/auth-provider";
 import { ThemeProvider } from "./theme/theme-provider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </ThemeProvider>
   </StrictMode>,
 );
