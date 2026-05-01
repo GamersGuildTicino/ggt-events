1ae358080c78fa23d3a1563ffda0a38e6d7ae377
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2026-05-01 14:07:59 +0200

    build: increase chunk size limit to 600 kB

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { defineConfig } from "vite";
 export default defineConfig(() => ({
   base: "/",
   build: {
+    chunkSizeWarningLimit: 600,
     rollupOptions: {
       output: {
         manualChunks: (id) => {
