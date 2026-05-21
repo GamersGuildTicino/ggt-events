38b92cb3bdc94a47b6164d41b57827e014bb7c97
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2026-05-21 15:31:05 +0200

    feat(fe): split chakra system file and add breakpoint

---
+++ src/theme/system.ts
@@ -0,0 +1,40 @@
+import { createSystem, defaultConfig } from "@chakra-ui/react";
+
+//------------------------------------------------------------------------------
+// System
+//------------------------------------------------------------------------------
+
+export const system = createSystem(defaultConfig, {
+  theme: {
+    breakpoints: {
+      xs: "15rem",
+    },
+    recipes: {
+      heading: {
+        variants: {
+          size: {
+            "2xl": { lineHeight: 1.2 },
+            "3xl": { lineHeight: 1.2 },
+            "lg": { lineHeight: 1.2 },
+            "md": { lineHeight: 1.2 },
+          },
+        },
+      },
+      text: {
+        base: {
+          lineHeight: 1.2,
+        },
+      },
+    },
+    tokens: {
+      colors: {
+        publicAccentBorder: { value: "#458db9" },
+        publicFooterBg: { value: "#0e4263" },
+        publicPageBg: { value: "#addcf9" },
+        publicSurfaceBg: { value: "#fafafa" },
+        publicSurfaceBorder: { value: "#65bff7" },
+        publicSurfaceMutedBg: { value: "#fafafa" },
+      },
+    },
+  },
+});
--- src/theme/theme-provider.tsx
+++ src/theme/theme-provider.tsx
@@ -1,48 +1,8 @@
-import {
-  ChakraProvider,
-  ClientOnly,
-  createSystem,
-  defaultConfig,
-} from "@chakra-ui/react";
+import { ChakraProvider, ClientOnly } from "@chakra-ui/react";
 import { ThemeProvider as NextThemesThemeProvider } from "next-themes";
 import { type ReactNode } from "react";
+import { system } from "./system";
 import { useTheme } from "./theme";
-
-//------------------------------------------------------------------------------
-// System
-//------------------------------------------------------------------------------
-
-const system = createSystem(defaultConfig, {
-  theme: {
-    recipes: {
-      heading: {
-        variants: {
-          size: {
-            "2xl": { lineHeight: 1.2 },
-            "3xl": { lineHeight: 1.2 },
-            "lg": { lineHeight: 1.2 },
-            "md": { lineHeight: 1.2 },
-          },
-        },
-      },
-      text: {
-        base: {
-          lineHeight: 1.2,
-        },
-      },
-    },
-    tokens: {
-      colors: {
-        publicAccentBorder: { value: "#458db9" },
-        publicFooterBg: { value: "#0e4263" },
-        publicPageBg: { value: "#addcf9" },
-        publicSurfaceBg: { value: "#fafafa" },
-        publicSurfaceBorder: { value: "#65bff7" },
-        publicSurfaceMutedBg: { value: "#fafafa" },
-      },
-    },
-  },
-});
 
 //------------------------------------------------------------------------------
 // Theme Provider
